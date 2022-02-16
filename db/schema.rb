38d91f74d8ac777e9b84200bf6f51efe9f452c8d
TristanCole <tristanleighcole@googlemail.com>
2022-02-16 11:15:30 -0700

    Implement proof of concept Tolkien API connection

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_14_010621) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_14_025950) do
   create_table "quotes", force: :cascade do |t|
     t.string "quote_content"
     t.string "quote_author"
     t.string "quote_image"
-ActiveRecord::Schema[7.0].define(version: 2022_02_14_025950) do
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
