ae1baea602c6b27fef3b60f5a6be4c848c6311ce
Gary <gary_markoski@hotmail.com>
2022-02-16 16:04:09 -0700

    re-added favourite code to favourite_controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,16 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!, only: :toggle_favorite
+
+
   def index
-    @favorites = Favorites.all
+    
+    user = User.find_by(id: session[:user_id] )
+
+    
+
+    if user 
+      @favorite_quotes = user.all_favorites
+    end  
   end
+
 end
