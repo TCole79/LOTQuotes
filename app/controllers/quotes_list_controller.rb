cff605482682e1c3b5a7344e150657a983ee18ee
Michael <kmiecik013@gmail.com>
2022-02-18 15:31:09 -0700

    addition of social media

--- app/controllers/quotes_list_controller.rb
+++ app/controllers/quotes_list_controller.rb
@@ -1,5 +1,7 @@
 class QuotesListController< ApplicationController
   def index
     @quotes = Quote.all
+    @sauronQuotes = Quote.where(quote_author: "Sauron")
+    @bilboQuotes = Quote.where(quote_author: "Bilbo Bagen")
   end
 end
