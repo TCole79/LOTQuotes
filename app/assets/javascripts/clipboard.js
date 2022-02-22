376644d512739dd30785f481c7c8c9f7baf9ee2d
TristanCole <tristanleighcole@googlemail.com>
2022-02-21 21:05:34 -0700

    Monday tweaks and minor fixes, inc. toasts + modal styling, fonts, backgrounds

--- app/assets/javascripts/clipboard.js
+++ app/assets/javascripts/clipboard.js
@@ -1,8 +1,11 @@
 // this code for the main page copy button
-const copyToClipboard = function() {
+const copyToClipboard = function () {
   const copyText = document.querySelector(".quote");
   navigator.clipboard.writeText(copyText.innerText).then(() => {
-    // Alert the user that the action took place.
-    alert("Copied to clipboard");
+    // Alert the user that the action took place via a toast
+    $("#toasty").toast("show");
+    setTimeout(() => {
+      $("#toasty").toast("hide");
+    }, 3000);
   });
 };
